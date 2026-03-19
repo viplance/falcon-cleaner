65f57e62e568bfa406c29bd0b425e02f7addc7a1
Dzmitry Sharko <viplance.com@gmail.com>
2026-03-19 18:55:48 +0300

    Initial Commit

---
+++ FalconCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FalconCleaner
+//
+//  Created by Dzmitry Sharko on 19.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FalconCleaner/FalconCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalconCleanerApp.swift
+//  FalconCleaner
+//
+//  Created by Dzmitry Sharko on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FalconCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
